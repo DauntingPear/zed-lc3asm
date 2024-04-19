95f972f1dbf1b76d73a5fe0963bf67a7930ce7d7
Adrian Brady <ajbrady999@protonmail.com>
2024-04-19 02:11:36 -0500

    update

---
+++ languages/asm/highlights.scm
@@ -0,0 +1,20 @@
+; General
+(label) @tag
+(reg) @variable.builtin
+
+; Comments
+[
+  (comment)
+] @comment @spell
+
+(reg) @operator
+
+; Literals
+(int) @number
+(hex) @number
+(string) @string
+
+; Keywords
+
+; Operators & Punctuation
+["," ":"] @punctuation.delimiter

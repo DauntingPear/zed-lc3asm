475d22588e13e686bc3620e88c92ffe2c5f5a0f7
Adrian Brady <ajbrady999@protonmail.com>
2024-04-29 11:50:29 -0500

    update highlighting scheme

--- languages/lc3asm/highlights.scm
+++ languages/lc3asm/highlights.scm
@@ -1,2 +1,21 @@
 ; General
-(label) @tag
+(label) @variable.special
+(hex) @number
+(int) @number
+(reg) @variable
+(label_jump) @type
+
+(meta [
+  (meta_ident) @keyword
+  (hex) @number
+])
+
+(instruction [
+  (trap) @keyword
+  (opcode) @function
+  (label) @variable.special
+])
+
+(string) @string
+
+(comment) @comment
